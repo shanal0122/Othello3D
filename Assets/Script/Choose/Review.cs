bbbf01ebbdc80be346f78d3cdfdfc9b8d41d8990
shanal0122 <sc314159sc@gmail.com>
2020-07-14 10:55:07 +0900

    Add Web Link of Review as Beta ver.

---
+++ Assets/Script/Choose/Review.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Review : MonoBehaviour
+{
+
+    public void OpenReview()
+    {
+      Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSe8WEYdxBFFnxRkGotibx-VfGlPFrJ6Ik5VTWN3MRh90nscRQ/viewform?usp=sf_link");
+    }
+}
